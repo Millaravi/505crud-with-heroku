69b41aec2c98581070d3c6110e131f5a38b5a568
yooyh939@google.com <yooyh939@gmail.com>
2017-05-05 07:28:05 +0000

    3

--- app/controllers/board_controller.rb
+++ app/controllers/board_controller.rb
@@ -12,25 +12,44 @@ class BoardController < ApplicationController
     @makeBoard = Post.new
     @makeBoard.title = params[:input_title]
     @makeBoard.editor = params[:input_editor]
+    @makeBoard.content = params[:input_content]
     @makeBoard.save
     
   end
 
   def show
     @Board = Post.all
-  
+    
   end
 
   def edit
-  
+    
+    edit_board = Post.find(params[:upd_board.id])
+    eidt_board.title = params[:input_title]
+    edit_board.editor = params[:input_editor]
+    edit_board.content = params[:input_content]
+    eidt_board.save
+    redirect_to '/'
+    
   end
   
   def update
-
-    redirect_to '/new'  
+    upd_id = params[:id]
+    upd_board = Post.find(upd_id)
+    upd_board.title = params[:input_title]
+    upd_board.editor = params[:input_editor]
+    upd_board.content = params[:input_content]
+    upd_board.save
+    
+    redirect_to '/'  
+  
   end
   
   def delete
-    redirect_to '/new'
+    del_id = params[:id]
+    del_board = Post.find(del_id)
+    del_board.delete
+    
+    redirect_to '/'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   root 'board#index'
+  
   get '/new' => 'board#new'
-  get 'my_board' => 'board#index'
+  get '/my_board' => 'board#index'
   post '/create' => 'board#create'
-
-  get 'show' => 'board#show'
-  get 'edit' => 'board#edit'
-
-  get 'board/edit'
+  
+  get '/show' => 'board#show'
+  get '/edit' => 'board#edit'
+  get '/delete/:id' => 'board#delete'
+  post '/update/:id' => 'board#update'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
